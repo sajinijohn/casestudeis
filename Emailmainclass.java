79c8e1f2ecbfbb4114df4bad18a1e7e2aeb91e57
sajinijohn <58158426+sajinijohn@users.noreply.github.com>
2019-12-12 14:31:18 +0530

    email updating program

---
+++ Emailmainclass.java
@@ -0,0 +1,71 @@
+package sajini;
+import java.util.Scanner;
+public class emailmain {
+public static void main(String args[])
+ {
+  Scanner ob=new Scanner(System.in);
+  int r;
+  String op1,op2;
+  email c[]=new email[20];//declaring array of objects
+  int i=0;
+  int id=111;
+  do
+   {
+     System.out.print("1.Registration\n2.Update mail\n3.Exit\n");
+     r=ob.nextInt();
+     switch(r)
+      {
+             //first case for registration
+        case 1:
+        System.out.println("Enter your name:");
+        String n=ob.next();
+        System.out.println("Enter your address:");
+        String a=ob.next();
+        System.out.println("Contact Number:");
+        String p=ob.next();
+        System.out.println("Email ID:");
+        String e=ob.next();
+        System.out.println("Enter proof type:");
+        String pt=ob.next();
+        System.out.println("Enter proof id:");
+        String pi=ob.next();
+        c[i]=new email();
+        c[i].register(id,n,a,p,e,pt,pi);
+        System.out.println("Thank you for registering.Your id is :"+id);
+        i++;
+        id++;
+        break;
+             //second case for updating email
+        case 2:
+        int k=0;
+        System.out.println("Update Email :");
+        System.out.println("Enter reg_ID :");
+        int rid=ob.nextInt();
+        for(int j=0;j<i;j++)
+         {
+           if(c[j].id==rid)
+            {
+             System.out.println("Enter new Email ID :");
+             e=ob.next();
+             c[j].update(e);
+             System.out.println("Email Updated");
+             c[j].display();
+             k=1;
+             break;
+         }
+    }
+    if(k==0)
+     {
+       System.out.println("Invalid registration id");
+     }
+     break;
+            //third case for exit
+     case 3:
+     System.exit(0);
+  }
+  System.out.println("Do you want to continue (yes/no)");
+  op1=ob.next();
+  }while((op1.equals("yes"))||(op1.equals("YES")||(op1.equals("Yes"))));
+ }
+}
+    
